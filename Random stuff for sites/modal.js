aa89d90a2c9e97e1e61e8e9641a8b95c9ba925ab
loliconya <48636381+loliconya@users.noreply.github.com>
2022-03-30 19:45:10 +1100

    Add files via upload

--- Random stuff for sites/modal.js
+++ Random stuff for sites/modal.js
@@ -11,17 +11,17 @@ const modals = () => {
                 }
                 // Показ модального окна
                 modal.style.display = 'block';
-                document.body.overflow = 'hidden';
+                document.body.style.overflow = 'hidden';
             });
         }));
         close.addEventListener('click', (e) => {
             modal.style.display = 'none';
-            document.body.overflow = 'unset';
+            document.body.style.overflow = 'unset';
         });
         modal.addEventListener('click', (e) => {
             if(e.target === modal) {
                 modal.style.display = 'none';
-                document.body.overflow = 'unset';
+                document.body.style.overflow = 'unset';
             }
         })
     };
@@ -29,12 +29,13 @@ const modals = () => {
     function showModal(selector, time) {
         setTimeout(() => {
             document.querySelector(selector).style.display = 'block';
-            document.body.overflow = 'hidden';
+            document.body.style.overflow = 'hidden';
         }, time);
     }
 
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
     bindModal('.phone_link', '.popup', '.popup .popup_close');
+    bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
 
     // showModal('.popup', 60000);
 }
